61f9a7f8a49b667bfd0f29d9e754cc63d0392ae3
11kara11 <beev.kirill@gmail.com>
2022-10-03 17:29:52 +0300

    Общая картина по классам и функциям
    описал функции и классы + requirements

---
+++ DateBase.py
@@ -0,0 +1,9 @@
+class DateBase():   # класс базы данных, тут осуществляется работа с значениями валют
+    def __init__(self):
+        pass
+
+    def read_quotes(self):  # читает котировки
+        pass
+
+    def ParsDate(self): # парсит новые значения
+        pass
---
+++ main.py
@@ -0,0 +1,7 @@
+class Beenance():   # главный класс с интерфейсом и простыми функциями
+    def __init__(self):
+        pass
+
+    def initUI(self):
+        pass
+
